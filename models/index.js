90192862a18a0c3378a5eb5d8407f78aed239e3a
Fadel Mahougnon SEWADE <fadelsew02@HP-ENVY-15-Notebook-PC-c186b2eb.localdomain>
2023-09-27 14:28:12 +0100

    script build ajouté

--- models/index.js
+++ models/index.js
@@ -1,9 +1,21 @@
+// const { Sequelize } = require('sequelize');
+
+// // Option 3: Passing parameters separately (other dialects)
+// const sequelize = new Sequelize('tpstores', 'fadelsew', 'azerty', {
+//   host: 'localhost',
+//   dialect: 'postgres' 
+// });
+
+// module.exports = sequelize;
+
 const { Sequelize } = require('sequelize');
 
-// Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('tpstores', 'fadelsew', 'azerty', {
-  host: 'localhost',
-  dialect: 'postgres' 
+// Récupérez les informations de connexion de Render
+const databaseUrl = process.env.DATABASE_URL; // Cela dépendra de ce que Render fournit
+
+// Utilisez l'URL de la base de données de Render pour la configuration
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: 'postgres',
 });
 
 module.exports = sequelize;
